822eb4040a7a68ce08e84aac817511c078427c72
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2020-07-06 06:21:57 +0700

    update launcher.dart create simple launcher layout

--- lib/launcher.dart
+++ lib/launcher.dart
@@ -1,18 +1,17 @@
 import 'dart:async';
 
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:election_flutter_app/login.dart';
 import 'package:flutter/material.dart';
 
-class Launcher extends StatefulWidget{
+class Launcher extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return LauncherScreen();
   }
-
 }
 
-class LauncherScreen extends State<Launcher>{
-
+class LauncherScreen extends State<Launcher> {
   @override
   void initState() {
     super.initState();
@@ -22,19 +21,45 @@ class LauncherScreen extends State<Launcher>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Launcher"),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/smk_taruna_bhakti.png",
+                width: 200,
+                height: 200,
+                alignment: Alignment.center,
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RotateAnimatedTextKit(
+                text: ["Select Your", "Future Leader"],
+                totalRepeatCount: 4,
+                pause: Duration(seconds: 1),
+                displayFullTextOnTap: true,
+                textStyle: TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold, color: Colors.blueAccent,),
+                textAlign: TextAlign.center,
+                alignment: Alignment.center,
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
 
   startLaunching() {
-    var duration = Duration(seconds: 2);
-    return Timer(duration, (){
-      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_){
+    var duration = Duration(seconds: 5);
+    return Timer(duration, () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) {
         return Login();
       }));
     });
   }
-
 }
