0a82122fd595bdea50a9fbf70cf240ad8e696505
quifar <42423920+RafiIzzatulRizquFaris@users.noreply.github.com>
2020-07-07 06:12:26 +0700

    create login_contract.dart

---
+++ lib/login_contract.dart
@@ -0,0 +1,8 @@
+abstract class LoginContractPresenter{
+  getLoginData (String email, String password){}
+  loadLoginData (String email, String password){}
+}
+
+abstract class LoginContractView{
+  setLoginData (String uid){}
+}
