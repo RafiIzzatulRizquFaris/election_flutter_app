c5c277b84acc283e8057231e0f86007a989aea0b
quifar <risqufaris@gmail.com>
2020-07-13 21:19:07 +0700

    update main.dart add checkUid method to check whether uid was already chosen or not

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:election_flutter_app/home.dart';
 import 'package:election_flutter_app/launcher.dart';
+import 'package:election_flutter_app/post.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -24,7 +26,14 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: token == null ? Launcher() : Home(),
+      home: token == null ? Launcher() : checkUid(token),
     );
+  }
+
+  checkUid(String token) async {
+    Firestore firestore = Firestore.instance;
+    QuerySnapshot querySnapshot = await firestore.collection("user").where("id_user", isEqualTo: token).getDocuments();
+    bool chosen = querySnapshot.documents[0].data["chosen"];
+    chosen ? Post() : Home();
   }
 }
