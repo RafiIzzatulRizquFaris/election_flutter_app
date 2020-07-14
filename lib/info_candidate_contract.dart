852a1f08373f7ec3f39640fc13f807ad4925c090
quifar <risqufaris@gmail.com>
2020-07-14 21:48:40 +0700

    Create info_candidate_contract.dart

---
+++ lib/info_candidate_contract.dart
@@ -0,0 +1,9 @@
+abstract class InfoCandidateContractPresenter{
+  getInfoCandidate(){}
+  loadData(){}
+}
+
+abstract class InfoCandidateContractView{
+  setInfoCandidate(List value){}
+  setOnErrorInfoCandidate(String error){}
+}
