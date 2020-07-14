a45dffc3093e3ffb894473ae043b6d3ce7ea1000
quifar <risqufaris@gmail.com>
2020-07-14 21:46:24 +0700

    update home.dart wrapping with container for background color

--- lib/home.dart
+++ lib/home.dart
@@ -1,21 +1,28 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatefulWidget{
+class Home extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return HomeScreen();
   }
-
 }
 
-class HomeScreen extends State<Home>{
+class HomeScreen extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Home"),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.blue, Colors.blueAccent],
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+          ),
+        ),
+        child: Center(
+          child: Text("Home", style: TextStyle(color: Colors.white, fontSize: 32),),
+        ),
       ),
     );
   }
-
 }
