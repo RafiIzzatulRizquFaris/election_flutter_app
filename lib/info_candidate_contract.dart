39aacb0c3ead24a3128056a66d2927ac4e8caf94
quifar <risqufaris@gmail.com>
2020-07-27 11:06:43 +0700

    Update info_candidate_contract.dart
    change setInfoCandidate parameter type to List<DocumentSnapshot>

--- lib/info_candidate_contract.dart
+++ lib/info_candidate_contract.dart
@@ -1,9 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 abstract class InfoCandidateContractPresenter{
   getInfoCandidate(){}
   loadData(){}
 }
 
 abstract class InfoCandidateContractView{
-  setInfoCandidate(List value){}
+  setInfoCandidate(List<DocumentSnapshot> value){}
   setOnErrorInfoCandidate(String error){}
 }
