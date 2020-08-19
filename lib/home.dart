5d8964040e598b1ae70d457b57b908c21c5cd550
quifar <risqufaris@gmail.com>
2020-08-19 13:10:24 +0700

    change layout in home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:election_flutter_app/info_candidate_contract.dart';
 import 'package:election_flutter_app/info_candidate_presenter.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_tindercard/flutter_tindercard.dart';
+import 'package:flutter/widgets.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -11,10 +11,12 @@ class Home extends StatefulWidget {
   }
 }
 
-class HomeScreen extends State<Home> with TickerProviderStateMixin implements InfoCandidateContractView {
+class HomeScreen extends State<Home>
+    with TickerProviderStateMixin
+    implements InfoCandidateContractView {
   InfoCandidatePresenter infoCandidatePresenter;
   List<DocumentSnapshot> infoCandidateList = List<DocumentSnapshot>();
-  CardController cardController;
+//  CardController cardController;
   var isLoading;
 
   HomeScreen() {
@@ -34,50 +36,58 @@ class HomeScreen extends State<Home> with TickerProviderStateMixin implements In
       body: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
-            colors: [Colors.blue, Colors.blueAccent],
+            colors: [
+              Colors.white,
+              Colors.white70,
+            ],
             begin: Alignment.topRight,
             end: Alignment.bottomLeft,
           ),
         ),
         child: infoCandidateList == null
             ? CircularProgressIndicator(
-                backgroundColor: Colors.white,
-              ):
-            Container(
-              child: infoCard(),
-            ),
-//        Center(
-//                child: Text(
-//                  infoCandidateList[0].data["candidate_photo"].toString(),
-//                  style: TextStyle(
-//                    color: Colors.white,
-//                    fontSize: 32,
-//                  ),
-//                ),
-//              ),
+                backgroundColor: Colors.blueAccent,
+              )
+            : Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                color: Colors.blue,
+                child: SafeArea(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: EdgeInsets.all(16),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          "Pilihanmu menentukan masa depan sekolahmu",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          padding: EdgeInsets.all(10),
+                          width: MediaQuery.of(context).size.width,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(30),
+                              topRight: Radius.circular(30),
+                            ),
+                          ),
+                          child: Text("Section Two",),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
       ),
-    );
-  }
-
-  infoCard() {
-    print(infoCandidateList.length);
-    return TinderSwapCard(
-      cardBuilder: (BuildContext context, int index){
-        return Card(
-          child: Image.network(infoCandidateList[index].data["candidate_photo"]),
-        );
-      },
-      totalNum: infoCandidateList.length,
-      stackNum: infoCandidateList.length,
-      cardController: cardController = CardController(),
-      swipeUpdateCallback: updateCallback,
-      swipeCompleteCallback: completeCallback,
-      orientation: AmassOrientation.BOTTOM,
-      swipeEdge: 4.0,
-      maxWidth: MediaQuery.of(context).size.width * 0.9,
-      maxHeight: MediaQuery.of(context).size.width * 0.9,
-      minWidth: MediaQuery.of(context).size.width * 0.8,
-      minHeight: MediaQuery.of(context).size.width * 0.8,
     );
   }
 
@@ -96,19 +106,42 @@ class HomeScreen extends State<Home> with TickerProviderStateMixin implements In
     print(error);
   }
 
-  void updateCallback(DragUpdateDetails details, Alignment align) {
-    if (align.x < 0) {
-      print("Card is LEFT swiping");
-    } else if (align.x > 0) {
-      print("Card is RIGHT swiping");
-    } else if (align.y > 0){
-      print("Card is TOP swiping");
-    } else if (align.y < 0){
-      print("Card is BOTTOM swiping");
-    }
-  }
-
-  void completeCallback(CardSwipeOrientation orientation, int index) {
-    print(index.toString());
-  }
+//  infoCard() {
+//    print(infoCandidateList.length);
+//    return TinderSwapCard(
+//      cardBuilder: (BuildContext context, int index) {
+//        return Card(
+//          child:
+//          Image.network(infoCandidateList[index].data["candidate_photo"]),
+//        );
+//      },
+//      totalNum: infoCandidateList.length,
+//      stackNum: infoCandidateList.length,
+//      cardController: cardController = CardController(),
+//      swipeUpdateCallback: updateCallback,
+//      swipeCompleteCallback: completeCallback,
+//      orientation: AmassOrientation.BOTTOM,
+//      swipeEdge: 4.0,
+//      maxWidth: MediaQuery.of(context).size.width * 0.9,
+//      maxHeight: MediaQuery.of(context).size.width * 0.9,
+//      minWidth: MediaQuery.of(context).size.width * 0.8,
+//      minHeight: MediaQuery.of(context).size.width * 0.8,
+//    );
+//  }
+//
+//  void updateCallback(DragUpdateDetails details, Alignment align) {
+//    if (align.x < 0) {
+//      print("Card is LEFT swiping");
+//    } else if (align.x > 0) {
+//      print("Card is RIGHT swiping");
+//    } else if (align.y > 0) {
+//      print("Card is TOP swiping");
+//    } else if (align.y < 0) {
+//      print("Card is BOTTOM swiping");
+//    }
+//  }
+//
+//  void completeCallback(CardSwipeOrientation orientation, int index) {
+//    print(index.toString());
+//  }
 }
