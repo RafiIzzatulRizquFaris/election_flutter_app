544f0222359c6cc283f2753824316c71793149fe
quifar <risqufaris@gmail.com>
2020-09-26 17:43:04 +0700

    Update post.dart

--- lib/post.dart
+++ lib/post.dart
@@ -1,3 +1,5 @@
+import 'package:election_flutter_app/app_color.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Post extends StatefulWidget {
@@ -11,9 +13,37 @@ class PostScreen extends State<Post> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColor().blueColor,
       body: Center(
-        child: Text(
-          "Post Screen",
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/check_success.png",
+              alignment: Alignment.center,
+              width: 150,
+              height: 150,
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20, bottom: 10),
+              child: Text(
+                "Congratulations",
+                style: TextStyle(
+                  fontSize: 32,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+            Text(
+              "Thank you for voting and not abstaining\nYour Vote Matters",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
         ),
       ),
     );
