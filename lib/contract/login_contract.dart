523f6ed36e03cfbdf146786cd2fb347b59bd742b
quifar <risqufaris@gmail.com>
2020-10-12 21:29:41 +0700

    Update login_contract.dart
    change parameter type to Login model

--- lib/contract/login_contract.dart
+++ lib/contract/login_contract.dart
@@ -1,9 +1,16 @@
-abstract class LoginContractPresenter{
+import 'package:election_flutter_app/model/Login.dart';
+
+class LoginContractPresenter{
   getLoginData (String email, String password){}
   loadLoginData (String email, String password){}
 }
 
-abstract class LoginContractView{
-  setLoginData (List value){}
-  onErrorLogin (String error){}
+class LoginContractView{
+  setLoginData (Login login){}
+  onErrorLogin (error){}
 }
+
+// abstract class LoginContractView{
+//   setLoginData (List value){}
+//   onErrorLogin (String error){}
+// }
