1a0b3d8b487420a0831a4056c248c5ec080af98f
quifar <risqufaris@gmail.com>
2020-10-12 21:27:45 +0700

    Create Vote.dart

---
+++ lib/model/Vote.dart
@@ -0,0 +1,20 @@
+class Vote {
+  String message;
+  String status;
+
+  Vote({this.message, this.status});
+
+  factory Vote.fromJson(Map<String, dynamic> json) {
+    return Vote(
+      message: json['message'],
+      status: json['status'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['message'] = this.message;
+    data['status'] = this.status;
+    return data;
+  }
+}
