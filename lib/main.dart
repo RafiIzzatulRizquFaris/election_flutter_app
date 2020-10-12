b99ce3c21f313e9000be0d2055537c450b7eb075
quifar <risqufaris@gmail.com>
2020-10-12 21:30:40 +0700

    Update main.dart
    change shared preferennces data and simplify main.dart code

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,22 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:election_flutter_app/countdown.dart';
 import 'package:election_flutter_app/launcher.dart';
-import 'package:election_flutter_app/post.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SharedPreferences preferences = await SharedPreferences.getInstance();
-  var token = preferences.get("uid");
+  var nik = preferences.get("nik");
+  var pw = preferences.get("password");
   runApp(MyApp(
-    token: token,
+    nik: nik,
+    password: pw,
   ));
 }
 
 class MyApp extends StatelessWidget {
-  final String token;
+  final String nik, password;
 
-  const MyApp({this.token});
+  const MyApp({this.nik, this.password});
 
   @override
   Widget build(BuildContext context) {
@@ -28,33 +27,33 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home:
-      token == null
-          ? Launcher()
-          : Container(
-              child: FutureBuilder(
-                future: isAlreadyChosen(),
-                builder: (context, snapshot) {
-                  print(snapshot.data);
-                  if(snapshot.data == false){
-                    return Countdown();
-                  }else if(snapshot.data == true){
-                    return Post();
-                  }else{
-                    return Countdown();
-                  }
-                },
-              ),
-            ),
+      home: Launcher(nik: nik, password: password,),
+      // token == null
+      //     ? Launcher()
+      //     : Container(
+      //         child: FutureBuilder(
+      //           future: isAlreadyChosen(),
+      //           builder: (context, snapshot) {
+      //             print(snapshot.data);
+      //             if(snapshot.data == false){
+      //               return Countdown();
+      //             }else if(snapshot.data == true){
+      //               return Post();
+      //             }else{
+      //               return Countdown();
+      //             }
+      //           },
+      //         ),
+      //       ),
     );
   }
 
-  Future<bool> isAlreadyChosen() async {
-    Firestore firestore = Firestore.instance;
-    QuerySnapshot querySnapshot = await firestore
-        .collection("user")
-        .where("iduser", isEqualTo: token)
-        .getDocuments();
-    return querySnapshot.documents[0].data["chosen"];
-  }
+  // Future<bool> isAlreadyChosen() async {
+  //   Firestore firestore = Firestore.instance;
+  //   QuerySnapshot querySnapshot = await firestore
+  //       .collection("user")
+  //       .where("iduser", isEqualTo: token)
+  //       .getDocuments();
+  //   return querySnapshot.documents[0].data["chosen"];
+  // }
 }
