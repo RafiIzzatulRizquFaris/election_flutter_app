f12cfddef82d776dc5dfdd48079ae38c48c630de
quifar <risqufaris@gmail.com>
2020-10-12 21:29:17 +0700

    Update login_presenter.dart
    change get data from firebase to local api

--- lib/presenter/login_presenter.dart
+++ lib/presenter/login_presenter.dart
@@ -1,6 +1,8 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:convert';
+
 import 'package:election_flutter_app/contract/login_contract.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:election_flutter_app/model/Login.dart';
+import 'package:http/http.dart';
 
 class LoginPresenter implements LoginContractPresenter {
   LoginContractView _loginContractView;
@@ -11,23 +13,35 @@ class LoginPresenter implements LoginContractPresenter {
   loadLoginData(String email, String password) {
     getLoginData(email, password)
         .then((value) => _loginContractView.setLoginData(value))
-        .catchError((err) => _loginContractView.onErrorLogin(err.toString()));
+        .catchError((err) => _loginContractView.onErrorLogin(err));
   }
 
   @override
-  Future<List> getLoginData(String email, String password) async {
-    final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
-    final Firestore _firestore = Firestore.instance;
-
-    AuthResult authResult = await _firebaseAuth.signInWithEmailAndPassword(
-        email: email, password: password);
-    FirebaseUser firebaseUser = authResult.user;
-    QuerySnapshot snapshot = await _firestore
-        .collection('user')
-        .where("iduser", isEqualTo: firebaseUser.uid)
-        .getDocuments();
-    bool chosen = snapshot.documents[0].data["chosen"];
-    print(chosen);
-    return [firebaseUser.uid, chosen];
+  Future<Login> getLoginData(String email, String password) async {
+    final url = "https://apiosis.000webhostapp.com/olection/api/Login";
+    Map<String, String> body = {
+      "nik" : email,
+      "password" : password,
+    };
+    Client client = Client();
+    final response = await client.post(url, body: body);
+    if(response.statusCode != 200){
+      print("Response is not Success");
+    }
+    var content = json.decode(response.body);
+    return Login.fromJson(content);
+    // final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+    // final Firestore _firestore = Firestore.instance;
+    //
+    // AuthResult authResult = await _firebaseAuth.signInWithEmailAndPassword(
+    //     email: email, password: password);
+    // FirebaseUser firebaseUser = authResult.user;
+    // QuerySnapshot snapshot = await _firestore
+    //     .collection('user')
+    //     .where("iduser", isEqualTo: firebaseUser.uid)
+    //     .getDocuments();
+    // bool chosen = snapshot.documents[0].data["chosen"];
+    // print(chosen);
+    // return [firebaseUser.uid, chosen];
   }
 }
