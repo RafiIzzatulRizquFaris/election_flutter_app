ae212388f2657aab6c17fd66c0cae179939ddd0d
quifar <risqufaris@gmail.com>
2020-10-13 21:06:35 +0700

    Create logout_presenter.dart

---
+++ lib/presenter/logout_presenter.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:election_flutter_app/contract/logout_contract.dart';
+import 'package:election_flutter_app/model/Logout.dart';
+import 'package:http/http.dart';
+
+class LogoutPresenter implements LogoutContractPresenter {
+  LogoutContractView _logoutContractView;
+
+  LogoutPresenter(this._logoutContractView);
+
+  @override
+  Future<Logout> getLogoutData(String nik, String password) async {
+    String url = "https://apiosis.000webhostapp.com/olection/api/Logout";
+    Client client = Client();
+    var response = await client.post(
+      url,
+      body: {
+        "nik": nik,
+        "password": password,
+      },
+    );
+    var json = jsonDecode(response.body);
+    return Logout.fromJson(json);
+  }
+
+  @override
+  loadLogoutData(String nik, String password) {
+    getLogoutData(nik, password)
+        .then((value) => _logoutContractView.setLogoutData(value))
+        .catchError((err) => _logoutContractView.setOnErrorLogout(err));
+  }
+}
