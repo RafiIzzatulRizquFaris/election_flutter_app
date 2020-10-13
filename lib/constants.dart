42b1dd863e9040ae32dc4b2faf4baaaab1591b4a
quifar <risqufaris@gmail.com>
2020-10-13 21:30:01 +0700

    rename app_color.dart to constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,3 +4,7 @@ class AppColor{
   final Color whiteColor = Color(0xffFAF8F6);
   final Color blueColor = Color(0xff5A00FF);
 }
+
+class UrlConst{
+  final String domain = "https://apiosis.000webhostapp.com/olection/api/";
+}
