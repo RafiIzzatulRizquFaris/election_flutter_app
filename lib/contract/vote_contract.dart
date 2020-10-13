0cb56476a79740514c493c2f92135cfe8c067d08
quifar <risqufaris@gmail.com>
2020-10-13 21:06:16 +0700

    Create vote_contract.dart

---
+++ lib/contract/vote_contract.dart
@@ -0,0 +1,11 @@
+import 'package:election_flutter_app/model/Vote.dart';
+
+class VoteContractView{
+  setVoteData(Vote vote){}
+  onErrorVote(error){}
+}
+
+class VoteContractPresenter{
+  getVoteData(String id){}
+  loadVoteData(String id){}
+}
